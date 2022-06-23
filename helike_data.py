addbe20d292dd485845ba02d62436aab369840ce
Maurice Leutenegger <mleutene@gs66-epistrophy.ndc.nasa.gov>
2022-06-23 16:48:47 -0400

    Add helike_data.py with dictionary of wavelengths

---
+++ helike_data.py
@@ -0,0 +1,21 @@
+dict helike_wavelengths {
+    #6: (), # need to look up wavelenghs for C
+    7: (1907.26, 1896.74),
+    8: (1638.28, 1623.61),
+    10: (1272.81, 1248.28),
+    12: (1034.31, 997.46),
+    14: (865.14, 814.69),
+    16: (738.32, 673.40)
+}
+# format is (lambda1, lambda2)
+
+# relative contribution of J=2 to effective branching ratio:
+dict helike_B2_B {
+    # 6: ,
+    7: 0.004,
+    8: 0.008,
+    10: 0.033,
+    12: 0.101,
+    14: 0.217,
+    16: 0.341,
+}
