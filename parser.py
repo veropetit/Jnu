0ac7f61afcf24f81026e5d07c3f021141102a482
veropetit <83323003+veropetit@users.noreply.github.com>
2022-06-27 20:03:59 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -152,7 +152,7 @@ class FASTWIND_model :
                 self.jwavelengths = np.append (self.jwavelengths, wl*u.AA)
                 self.jnu = np.append (self.jnu, j*Fnu_unit(), axis=0) # check correct axis
         # Calculating J_lambda
-        #self.jlambda = np.zeros(self.jwavelengths.size, self.radius.size)*Flambda_unit()
+        self.jlambda = np.zeros((self.jwavelengths.size, self.radius.size))*Flambda_unit()
         #for i in range(0,self.jwavelengths.size):
         #    self.jlambda[i,:] = (self.jnu[i,:] * const.c / self.jwavelengths[i]**2).to(Flambda_unit())
         
